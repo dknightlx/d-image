de39647a55c749bcf0bbb0828b7f43cc24d5a092
DK <liuxu@souche.com>
2017-12-03 11:38:33 +0800

    update

--- content/content.js
+++ content/content.js
@@ -1,10 +1,9 @@
 
 console.log('load', 'chrome');
-var imgList = document.getElementsByTagName('img');
+var imgList = document.querySelectorAll('img');
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log(message, sender, sendResponse);
     if (message.status === 'start') {
-        var imgList = document.getElementsByTagName('img');
         var imgSrcList = [];
         for (var i = 0; i < imgList.length; i++) {
             imgSrcList.push(imgList[i].getAttribute('src'));
